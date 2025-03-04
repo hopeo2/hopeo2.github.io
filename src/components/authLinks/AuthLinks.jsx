b9ac6d7d394442e9b5a4ea7b06b22e921345926f
killedbyIslam <irikilledbyislam@gmail.com>
2025-03-04 13:24:31 +0330

    hamberger

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -4,12 +4,11 @@ import styles from "./authLinks.module.css";
 import { useState } from "react";
 
 const AuthLinks = () => {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-
-  return (
-    <>
-      {/* {status === "unauthenticated" ? (
+    return (
+        <>
+            {/* {status === "unauthenticated" ? (
         <Link href="/login" className={styles.link}>
           Login
         </Link>
@@ -23,17 +22,23 @@ const AuthLinks = () => {
           </span>
         </>
       )} */}
-      <div className={styles.burger} onClick={() => setOpen(!open)}>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
-      </div>
-      {open && (
-        <div className={styles.responsiveMenu}>
-          <Link href="/" onClick={() => setOpen(!open)}>$~خانه</Link>
-          <Link href="/about" onClick={() => setOpen(!open)}>$~درباره من</Link>
-          <Link href="/contact" onClick={() => setOpen(!open)}>$~تماس با من</Link>
-          {/* {status === "notauthenticated" ? (
+            <div className={styles.burger} onClick={() => setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            {open && (
+                <div className={styles.responsiveMenu}>
+                    <Link href="/" onClick={() => setOpen(!open)}>
+                        $~خانه
+                    </Link>
+                    <Link href="/about" onClick={() => setOpen(!open)}>
+                        $~درباره من
+                    </Link>
+                    <Link href="/contact" onClick={() => setOpen(!open)}>
+                        $~تماس با من
+                    </Link>
+                    {/* {status === "notauthenticated" ? (
             <Link href="/login" onClick={() => setOpen(!open)}>Login</Link>
           ) : (
             <>
@@ -41,10 +46,24 @@ const AuthLinks = () => {
               <span className={styles.link}>Logout</span>
             </>
           )} */}
-        </div>
-      )}
-    </>
-  );
+                    <img
+                        src="/assets/images/bg-outlines.svg"
+                        width={960}
+                        height={380}
+                        alt="outline"
+                        className="relative z-2"
+                    />
+                    <img
+                        src="/assets/images/bg-outlines-fill.png"
+                        width={960}
+                        height={380}
+                        alt="outline"
+                        className="absolute inset-0 mix-blend-soft-light opacity-5"
+                    />
+                </div>
+            )}
+        </>
+    );
 };
 
 export default AuthLinks;
